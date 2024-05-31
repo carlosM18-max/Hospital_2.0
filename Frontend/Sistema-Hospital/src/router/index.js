ad18e62ebd1d2b7d55d96ba5a44f2574dbcfa340
carlosM18-max <carloshernandez74199@gmail.com>
2024-05-31 14:15:59 -0600

    Creacion de los formularios de login y register, ademas de la incorporacion de un sidebar

--- Frontend/Sistema-Hospital/src/router/index.js
+++ Frontend/Sistema-Hospital/src/router/index.js
@@ -1,20 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '@/components/Login.vue'
+import RegisterView from '@/components/Register.vue'
+import SidebarView from '@/components/Sidebar.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path: '/side-bar',
+      name: 'sidebar',
+      component: SidebarView
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     }
   ]
