a410e9b2a8635e5e95863043a16fa807af02f686
carlosM18-max <carloshernandez74199@gmail.com>
2024-06-20 15:15:37 -0600

    Creacion de metodo POST

--- Backend/routes/person.py
+++ Backend/routes/person.py
@@ -1,8 +1,37 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
 
 person = APIRouter()
+persons = []
 
+class model_person(BaseModel):
+    id:int
+    name:str
+    last_f_name:str
+    last_s_name:Optional[str]
+    age: int
+    birthday: datetime
+    curp: str
+    type_blood: str
+    created_at: datetime = datetime.now()
+    status: bool = False
+
+# Principal route
+@person.get("/")
+
+def welcom():
+    return "Bienvenido al API del sistema"
+
+# Second route
 @person.get("/person")
 
-def helloworld():
-    return "Hello world"
+def get_person():
+    return persons
+
+@person.post("/person")
+
+def save_person(data_person: model_person):
+    persons.append(data_person)
+    return "Datos guardados correectamente"
