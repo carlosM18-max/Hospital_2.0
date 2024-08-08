e6547a14436b8d5060a7a73ec1d07b254a4fa729
Alyn.Fosado <210764@utxicotepec.edu.mx>
2024-08-08 14:48:22 -0600

    nueva actualizacion de frontend tbc_organos

--- Frontend/Sistema-Hospital/src/components/RequestTransplant.vue
+++ Frontend/Sistema-Hospital/src/components/RequestTransplant.vue
@@ -1,135 +1,77 @@
 <template>
     <form>
-        <div class="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 rounded-lg shadow-lg overflow-hidden">
+        <div class="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 rounded-lg shadow-lg overflow-hidden mt-5">
             <!-- Done Information -->
-            <div class="flex justify-between flex flex-col items-center">
-                <label class="block text-gray-700 text-3xl font-normal mb-2">Informacion del Donatario</label>
+            <div class="flex justify-between  flex-col items-center">
+                <label class="block text-gray-700 text-3xl font-normal mb-2">Informacion del Solicitante</label>
             </div>
             <div class="mt-4">
                 <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Seleccione una Curp</label>
+                    <label class="block text-gray-700 text-sm font-bold mb-2">Seleccione a un Paciente</label>
                 </div>
                 <select
                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center">
-                    <option value="">-- Seleciona una CURP --</option>
+                    <option value="">-- Selecciona una Opcion --</option>
+                </select>
+            </div>
+            <div class="mt-4">
+                <div class="flex justify-between">
+                    <label class="block text-gray-700 text-sm font-bold mb-2">Seleccione a un Medico</label>
+                </div>
+                <select
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center">
+                    <option value="">-- Selecciona una Opcion --</option>
+                </select>
+            </div>
+            <div class="mt-4">
+                <div class="flex justify-between">
+                    <label class="block text-gray-700 text-sm font-bold mb-2">Seleccione un Servicio</label>
+                </div>
+                <select
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center">
+                    <option value="">-- Selecciona una Opcion --</option>
                 </select>
             </div>
             <!-- Doner Information -->
-            <div class="flex justify-between flex flex-col items-center mt-5">
-                <label class="block text-gray-700 text-3xl font-normal mb-2">Detalles de la Donacion</label>
+            <div class="flex justify-between  flex-col items-center mt-5">
+                <label class="block text-gray-700 text-3xl font-normal mb-2">Detalles de la Solicitud</label>
             </div>
             <div class="mt-4">
                 <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Seleccione una Curp</label>
+                    <label class="block text-gray-700 text-sm font-bold mb-2">Prioridad</label>
                 </div>
                 <select
                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center">
-                    <option value="">-- Seleciona una CURP --</option>
+                    <option value="">-- Selecciona una Opcion --</option>
                 </select>
             </div>
             <div class="mt-4">
                 <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Seleccione una Curp Medica</label>
+                    <label class="block text-gray-700 text-sm font-bold mb-2">Descripcion</label>
                 </div>
-                <select
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center">
-                    <option value="">-- Seleciona una CURP --</option>
-                </select>
-            </div>
-            <div class="mt-4">
-                <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Organo</label>
-                </div>
-                <select
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center">
-                    <option value="">-- Seleciona un Organo --</option>
-                </select>
-            </div>
-            <div class="mt-4">
-                <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Selecciona la Prioridad</label>
-                </div>
-                <select
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center">
-                    <option value="">-- Prioridad --</option>
-                </select>
-            </div>
-            <div class="mt-4">
-                <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Fecha de Registro</label>
-                </div>
-                <input
-                    class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-                    type="date" />
-            </div>
-            <div class="mt-4">
-                <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Hora de Registro</label>
-                </div>
-                <input
-                    class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-                    type="time" />
-            </div>
-            <!-- Summary Information -->
-            <div class="flex justify-between flex flex-col items-center mt-5">
-                <label class="block text-gray-700 text-3xl font-normal mb-2">Resumen de la Donacion</label>
-            </div>
-            <div class="mt-4">
-                <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Nombre del Donador</label>
-                </div>
-                <input
+                <textarea
                     class=" bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-                    type="text" disabled />
+                    type="text"  />
             </div>
             <div class="mt-4">
                 <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Nombre del Donatario</label>
+                    <label class="block text-gray-700 text-sm font-bold mb-2">Situacion o Estatus</label>
                 </div>
-                <input
-                    class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-                    type="text" disabled />
+                <select
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center">
+                    <option value="">-- Selecciona una Opcion --</option>
+                </select>
             </div>
             <div class="mt-4">
                 <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Nombre del Medico</label>
+                    <label class="block text-gray-700 text-sm font-bold mb-2">Aprobacion</label>
                 </div>
-                <input
-                    class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-                    type="text" disabled />
+                <select
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-center">
+                    <option value="">-- Selecciona una Opcion --</option>
+                </select>
             </div>
-            <div class="mt-4">
-                <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Organo</label>
-                </div>
-                <input
-                    class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-                    type="text" disabled />
-            </div>
-            <div class="mt-4">
-                <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Prioidad</label>
-                </div>
-                <input
-                    class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-                    type="text" disabled />
-            </div>
-            <div class="mt-4">
-                <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Fecha de Registro</label>
-                </div>
-                <input
-                    class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-                    type="text" disabled />
-            </div>
-            <div class="mt-4">
-                <div class="flex justify-between">
-                    <label class="block text-gray-700 text-sm font-bold mb-2">Hora de Registro</label>
-                </div>
-                <input
-                    class="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"
-                    type="text" disabled />
-            </div>
+
             <!-- Send Form -->
             <div class="mt-8">
                 <RouterLink to="/TableTransplante">
