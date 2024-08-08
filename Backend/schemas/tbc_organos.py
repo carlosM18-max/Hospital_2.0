d96afa6fff7aecd9f2036b88bbf8fdf58b42e49e
Alyn.Fosado <210764@utxicotepec.edu.mx>
2024-08-08 14:40:17 -0600

    Creacion de la carpeta schemas integrando el archivo tbc_organos

---
+++ Backend/schemas/tbc_organos.py
@@ -0,0 +1,48 @@
+from typing import List, Union
+from pydantic import BaseModel
+from datetime import datetime
+from enum import Enum
+
+class AparatoSistemaEnum(str, Enum):
+    Circulatorio = 'Circulatorio'
+    Digestivo = 'Digestivo'
+    Respiratorio = 'Respiratorio'
+    Nervioso = 'Nervioso'
+    Muscular = 'Muscular'
+    Esqueletico = 'Esquelético'
+    Endocrino = 'Endocrino'
+    Linfatico = 'Linfático'
+    Inmunologico = 'Inmunológico'
+    Reproductor = 'Reproductor'
+    Urinario = 'Urinario'
+    Sensorial = 'Sensorial'
+
+class DisponibilidadEnum(str, Enum):
+    Disponible = 'Disponible'
+    NoDisponible = 'No Disponible'
+    Reservado = 'Reservado'
+
+class TipoEnum(str, Enum):
+    Vital = 'Vital'
+    NoVital = 'No Vital'
+
+class OrganoBase(BaseModel):
+    Nombre: str
+    Aparato_Sistema: AparatoSistemaEnum
+    Descripcion: str
+    Disponibilidad: DisponibilidadEnum
+    Tipo: TipoEnum
+    Estatus: bool
+    Fecha_Registro: datetime
+    Fecha_Actualizacion: datetime
+
+class OrganoCreate(OrganoBase):
+    pass
+
+class OrganoUpdate(OrganoBase):
+    pass
+
+class Organo(OrganoBase):
+    ID: int
+    class Config:
+        orm_mode = True
