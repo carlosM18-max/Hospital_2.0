ab15e1f532f765b27b13541ce3128be8100ab62b
alyn.fosado <210764@utxicotepec.edu.mx>
2024-08-16 13:45:42 -0600

    integracion del portador para validacion de datos con token

--- Backend/routes/tbc_organos.py
+++ Backend/routes/tbc_organos.py
@@ -4,6 +4,8 @@ import crud.tbc_organos, config.db
 import schemas.tbc_organos
 import models.tbc_organos
 from typing import List
+from portadortoken import portador
+
 
 tbc_organos = APIRouter()
 
@@ -18,33 +20,33 @@ def get_db():
         
 
 
-@tbc_organos.get("/organos/", response_model=List[schemas.tbc_organos.Organo], tags=["Organos"])
+@tbc_organos.get("/organos/", response_model=List[schemas.tbc_organos.Organo], tags=["Organos"], dependencies=[Depends(portador())])
 def read_organos(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     db_organos = crud.tbc_organos.get_organos(db=db, skip=skip, limit=limit)
     return db_organos
 
-@tbc_organos.get("/organo/{id}", response_model=schemas.tbc_organos.Organo, tags=["Organos"])
+@tbc_organos.get("/organo/{id}", response_model=schemas.tbc_organos.Organo, tags=["Organos"], dependencies=[Depends(portador())])
 def read_organo(id: int, db: Session = Depends(get_db)):
     db_organo = crud.tbc_organos.get_organo(db=db, id=id)
     if db_organo is None:
         raise HTTPException(status_code=404, detail="Organo no encontrado")
     return db_organo
 
-@tbc_organos.post("/organo/", response_model=schemas.tbc_organos.Organo, tags=["Organos"])
+@tbc_organos.post("/organo/", response_model=schemas.tbc_organos.Organo, tags=["Organos"], dependencies=[Depends(portador())])
 def create_organo(organo: schemas.tbc_organos.OrganoCreate, db: Session = Depends(get_db)):
     db_organo = crud.tbc_organos.get_organo_by_nombre(db, nombre=organo.Nombre)
     if db_organo:
         raise HTTPException(status_code=400, detail="Organo existente intenta nuevamente")
     return crud.tbc_organos.create_organo(db=db, organo=organo)
 
-@tbc_organos.put("/organo/{id}", response_model=schemas.tbc_organos.Organo, tags=["Organos"])
+@tbc_organos.put("/organo/{id}", response_model=schemas.tbc_organos.Organo, tags=["Organos"], dependencies=[Depends(portador())])
 def update_organo(id: int, organo: schemas.tbc_organos.OrganoUpdate, db: Session = Depends(get_db)):
     db_organo = crud.tbc_organos.update_organo(db=db, id=id, organo=organo)
     if db_organo is None:
         raise HTTPException(status_code=404, detail="Organo no existe, no actualizado")
     return db_organo
 
-@tbc_organos.delete("/organo/{id}", response_model=schemas.tbc_organos.Organo, tags=["Organos"])
+@tbc_organos.delete("/organo/{id}", response_model=schemas.tbc_organos.Organo, tags=["Organos"], dependencies=[Depends(portador())])
 def delete_organo(id: int, db: Session = Depends(get_db)):
     db_organo = crud.tbc_organos.delete_organo(db=db, id=id)
     if db_organo is None:
